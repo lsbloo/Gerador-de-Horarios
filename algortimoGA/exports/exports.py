eb6945298ba35d20efb5e7c8730814098f7bc105
osvaldoairon <osvaldo_cavalcanti_filho@hotmail.com>
2020-07-20 17:51:17 -0300

    exports_02

---
+++ algortimoGA/exports/exports.py
@@ -0,0 +1,3 @@
+## modulo responsavel pelo processo de exportação dos quadros gerados do melhor individuo.
+## tem como responsabilidade exportar os arquivos csv correspondente aos cursos, como também o grafico de valores do melhor individuo.
+
